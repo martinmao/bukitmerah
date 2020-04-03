fde178f63d90712fe7fc33b711d23d90999e0292
Martin Mao <martinmao@icloud.com>
2020-04-03 11:00:58 +0800

    commit

--- ball-crud/src/main/java/org/scleropages/crud/dao/JooqRepository.java
+++ ball-crud/src/main/java/org/scleropages/crud/dao/JooqRepository.java
@@ -15,12 +15,23 @@
  */
 package org.scleropages.crud.dao;
 
+import org.apache.commons.lang3.StringUtils;
 import org.jooq.DSLContext;
+import org.jooq.SelectLimitPercentAfterOffsetStep;
+import org.jooq.SelectLimitStep;
 import org.scleropages.crud.FrameworkContext;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.support.PageableExecutionUtils;
+import org.springframework.util.Assert;
+
+import java.util.List;
+
+import static org.jooq.impl.DSL.*;
 
 /**
  * Support for jOOQ.
- * This interface is complement for {@link org.scleropages.crud.orm.jpa.GenericRepository}. GenericRepository implementations can lookup {@link DSLContext} by {@link #dslContext()}
+ * This interface is complement for {@link org.scleropages.crud.orm.jpa.GenericRepository}.
  *
  * @author <a href="mailto:martinmao@icloud.com">Martin Mao</a>
  */
@@ -34,5 +45,41 @@ public interface JooqRepository {
      */
     default DSLContext dslContext() {
         return FrameworkContext.getBean(DSLContext.class);
+    }
+
+    /**
+     * Apply spring data {@link Pageable} to current select.
+     *
+     * @param select
+     * @param pageable
+     * @return
+     */
+    default SelectLimitPercentAfterOffsetStep applyPageable(SelectLimitStep select, Pageable pageable) {
+        return select.offset(pageable.getOffset()).limit(pageable.getPageSize());
+    }
+
+    /**
+     * Create spring data {@link Page}.
+     *
+     * @param content query results.
+     * @param pageable
+     * @param select
+     * @param <T>
+     * @return
+     */
+    default <T> Page<T> createPage(List<T> content, Pageable pageable, SelectLimitStep select) {
+        Assert.notNull(select, "select must not be null.");
+        return PageableExecutionUtils.getPage(content, pageable, () -> {
+            String sql = select.getSQL().toLowerCase();
+            String[] splitByFirstFrom = org.springframework.util.StringUtils.split(sql, "from");
+            Assert.notNull(splitByFirstFrom, "invalid sql. can not split by 'from' fragment. ");
+            sql = " from " + splitByFirstFrom[1];
+            sql = StringUtils.substringBefore(sql, "order by");
+            String selectFragment = splitByFirstFrom[0].contains("distinct") ?
+                    StringUtils.replace(splitByFirstFrom[0], "select", "select count(") + ")" :
+                    "select count(*)";
+            sql = selectFragment + sql;
+            return (Long) dslContext().fetchOne(sql, select.getBindValues()).get(0);
+        });
     }
 }
