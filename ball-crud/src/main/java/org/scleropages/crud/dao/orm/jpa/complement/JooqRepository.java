d252b50ab8074421924fe190d82a06d17d563b39
Martin Mao <martinmao@icloud.com>
2020-05-08 15:27:22 +0800

    commit

--- ball-crud/src/main/java/org/scleropages/crud/dao/orm/jpa/complement/JooqRepository.java
+++ ball-crud/src/main/java/org/scleropages/crud/dao/orm/jpa/complement/JooqRepository.java
@@ -216,8 +216,9 @@ public interface JooqRepository<T extends Table, R extends Record, E> {
      * @return
      */
     default Page<? extends Record> dslPage(Supplier<SelectQuery> select, Pageable pageable, boolean useCountWrapped, boolean applySort) {
-        dslPageable(select, pageable, applySort);
-        return dslPage(select.get().fetch(), pageable, select, useCountWrapped);
+        SelectQuery<Record> countQuery = dslContext().selectFrom(select.get().getSQL()).getQuery();//create query for count.
+        dslPageable(select, pageable, applySort);//apply spring data pageable to source query.
+        return dslPage(select.get().fetch(), pageable, () -> countQuery, useCountWrapped);
     }
 
 
