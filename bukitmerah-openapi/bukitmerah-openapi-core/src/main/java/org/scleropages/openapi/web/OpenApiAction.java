f785c542c180a60ee5e7cd13d6a8e3ed4ff56155
Martin Mao <martinmao@icloud.com>
2020-06-19 16:21:21 +0800

    c

--- bukitmerah-openapi/bukitmerah-openapi-core/src/main/java/org/scleropages/openapi/web/OpenApiAction.java
+++ bukitmerah-openapi/bukitmerah-openapi-core/src/main/java/org/scleropages/openapi/web/OpenApiAction.java
@@ -31,7 +31,6 @@ import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
